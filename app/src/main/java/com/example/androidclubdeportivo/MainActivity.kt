68ce1022c3a81386c0aefeb560e502a7f825a9e0
Daniela Homobono <110701158+danielaHomobono@users.noreply.github.com>
2024-09-23 11:06:54 -0300

    primra activity

--- app/src/main/java/com/example/androidclubdeportivo/MainActivity.kt
+++ app/src/main/java/com/example/androidclubdeportivo/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.androidclubdeportivo
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        // Referencia al Spinner en el layout
+        val spinnerDocumentType: Spinner = findViewById(R.id.spinnerDocumentType)
+
+        // Crear el ArrayAdapter con los valores del array de strings
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.document_type_array,  // Referencia al array en strings.xml
+            android.R.layout.simple_spinner_item
+        )
+
+        // Especificar el layout para las opciones desplegables del Spinner
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Asignar el adapter al Spinner
+        spinnerDocumentType.adapter = adapter
     }
 }
